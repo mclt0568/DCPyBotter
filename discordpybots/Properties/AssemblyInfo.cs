79f41d2d4e744dbbf2409a7d4804a2c025eb4912
Frankium <tis7bfrankie@gmail.com>
2020-02-20 16:23:50 +0800

    Added Lisence
    MIT Lisence. Yay Open Source.

--- discordpybots/Properties/AssemblyInfo.cs
+++ discordpybots/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DCPyBotter")]
-[assembly: AssemblyCopyright("Copyright ©mclt0568  2020")]
+[assembly: AssemblyCopyright("Copyright ©mclt0568 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
